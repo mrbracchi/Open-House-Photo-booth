28eca0830c00523cc337a1a3b4f6b9c99b753dca
mrbracchi <42683116+mrbracchi@users.noreply.github.com>
2018-08-25 21:11:45 -0500

    starting the auto folder creation

---
+++ folder_creation.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+##goto user's  document directory
+
+cd ~/Documents
+
+#make folders
+
+mkdir OHPB
+
+#enter directory
+cd ~/Documents/OHPB
+
+#create folders for water marks, images and gifs
+mkdir wm images gifs
