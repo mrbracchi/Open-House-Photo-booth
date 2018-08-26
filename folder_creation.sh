5f3c84a1d215dff09e24a1eece82987f138ee0f5
mrbracchi <42683116+mrbracchi@users.noreply.github.com>
2018-08-25 21:32:24 -0500

    Update folder_creation.sh

--- folder_creation.sh
+++ folder_creation.sh
@@ -11,5 +11,5 @@ mkdir OHPB
 #enter directory
 cd ~/Documents/OHPB
 
-#create folders for water marks, images and gifs
-mkdir wm images gifs
+#create folders for water marks, images gifs and overlay
+mkdir wm images gifs overlays
